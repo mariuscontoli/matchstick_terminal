826d0d7de670b7f4cd43ddf1e3e1630575014725
mariuscontoli <marius.contoli-legay@epitech.eu>
2020-01-19 14:12:08 +0100

    added some protos

--- include/matchstick.h
+++ include/matchstick.h
@@ -14,13 +14,41 @@
 #include <string.h>
 #include "../my_printf/include/my_printf.h"
 
-char **my_square(int number, int number2);
+typedef struct matchstick
+{
+    char **map;
+    int stop;
+    int length;
+    int offset;
+    int numb_lines;
+    int len_line;
+    int len_map;
+    int height;
+    int error;
+    int current_line;
+    int match_left;
+    int step;
+}*matchstick_t;
+
+void my_square(matchstick_t match);
+void fill_with_matches(matchstick_t match);
+void my_remove(matchstick_t match, int line, int matches);
+void game(matchstick_t match);
+void input(matchstick_t match);
+void display_map(char **tab);
+int input_line(matchstick_t match, int step);
+int input_matches(matchstick_t match);
+int is_valid_line(matchstick_t match, char *str);
+int is_valid_input(char *str);
+int is_valid_matche(matchstick_t match, char *str);
+int how_many_matches(matchstick_t match);
+int how_many_matches_line(matchstick_t match, int line);
+int error_line(matchstick_t match, int line);
+int error_matche(matchstick_t match, int matche);
+int devil_error(matchstick_t match, int matches);
 int how_much_letters(char const *str, int x);
 int how_much_words(char const *str);
-char *my_strclean(char *dest, char const *src);
-int avoid_sep(char const *str, int i);
 int my_getnbr(char *str);
-int is_alpha(char c);
 int my_strlen(char const *str);
 
 #endif /* !MATCHSTICK_H_ */
